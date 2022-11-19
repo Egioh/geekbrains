7a1581b006c9f935e952c0c8923cbc044f3e22af
Pavel <pavelatavin@yandex.ru>
2022-11-19 18:49:41 +0500

    next try

---
+++ examples/example 4 div/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine ("введите первое число");
+double firstNumber =  (6);
+Console.WriteLine ("Введите второе число");
+double secondNumber = (5);
+Console.WriteLine (firstNumber/secondNumber);
